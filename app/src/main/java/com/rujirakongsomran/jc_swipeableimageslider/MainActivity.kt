7a12361e0e0274ce4ba5da28b3482634190d0748
Rujira Kongsomran <rujirabiwberry@gmail.com>
2023-07-07 21:02:32 +0700

    Custom UI show index of image

--- app/src/main/java/com/rujirakongsomran/jc_swipeableimageslider/MainActivity.kt
+++ app/src/main/java/com/rujirakongsomran/jc_swipeableimageslider/MainActivity.kt
@@ -21,6 +21,7 @@ import androidx.compose.material.icons.filled.KeyboardArrowRight
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -60,48 +61,58 @@ class MainActivity : ComponentActivity() {
                             contentScale = ContentScale.Crop,
                             modifier = Modifier.fillMaxSize()
                         )
-                    }
-                    Box(
-                        modifier = Modifier
-                            .offset(y = -(16).dp)
-                            .fillMaxWidth(0.5f)
-                            .clip(RoundedCornerShape(100))
-                            .background(MaterialTheme.colorScheme.background)
-                            .padding(8.dp)
-                            .align(Alignment.BottomCenter)
-                    ) {
-                        IconButton(
-                            onClick = {
-                                scope.launch {
-                                    pagerState.animateScrollToPage(
-                                        pagerState.currentPage - 1
-                                    )
-                                }
-                            },
-                            modifier = Modifier.align(Alignment.CenterStart)
+                        Box(
+                            modifier = Modifier
+                                .offset(y = -(16).dp, x = -(16).dp)
+                                .clip(RoundedCornerShape(20))
+                                .background(MaterialTheme.colorScheme.background)
+                                .align(Alignment.BottomEnd)
+                                .padding(8.dp)
                         ) {
-                            Icon(
-                                imageVector = Icons.Default.KeyboardArrowLeft,
-                                contentDescription = "Go back"
-                            )
-
-                        }
-                        IconButton(
-                            onClick = {
-                                scope.launch {
-                                    pagerState.animateScrollToPage(
-                                        pagerState.currentPage + 1
-                                    )
-                                }
-                            },
-                            modifier = Modifier.align(Alignment.CenterEnd)
-                        ) {
-                            Icon(
-                                imageVector = Icons.Default.KeyboardArrowRight,
-                                contentDescription = "Go forward"
-                            )
+                            Text(text = "$index")
                         }
                     }
+//                    Box(
+//                        modifier = Modifier
+//                            .offset(y = -(16).dp)
+//                            .fillMaxWidth(0.5f)
+//                            .clip(RoundedCornerShape(100))
+//                            .background(MaterialTheme.colorScheme.background)
+//                            .padding(8.dp)
+//                            .align(Alignment.BottomCenter)
+//                    ) {
+//                        IconButton(
+//                            onClick = {
+//                                scope.launch {
+//                                    pagerState.animateScrollToPage(
+//                                        pagerState.currentPage - 1
+//                                    )
+//                                }
+//                            },
+//                            modifier = Modifier.align(Alignment.CenterStart)
+//                        ) {
+//                            Icon(
+//                                imageVector = Icons.Default.KeyboardArrowLeft,
+//                                contentDescription = "Go back"
+//                            )
+//
+//                        }
+//                        IconButton(
+//                            onClick = {
+//                                scope.launch {
+//                                    pagerState.animateScrollToPage(
+//                                        pagerState.currentPage + 1
+//                                    )
+//                                }
+//                            },
+//                            modifier = Modifier.align(Alignment.CenterEnd)
+//                        ) {
+//                            Icon(
+//                                imageVector = Icons.Default.KeyboardArrowRight,
+//                                contentDescription = "Go forward"
+//                            )
+//                        }
+//                    }
                 }
             }
         }
